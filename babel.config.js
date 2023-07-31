927da0101799b6ab181a8eb40c02fe58137babf2
RyuyaGohara <138365197+RyuyaGohara@users.noreply.github.com>
2023-07-31 10:46:23 +0000

    応用課題1-2

--- babel.config.js
+++ babel.config.js
@@ -54,6 +54,18 @@ module.exports = function(api) {
         }
       ],
       [
+        '@babel/plugin-transform-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-proposal-private-property-in-object',
+        {
+          loose: true
+        }
+      ],
+      [
         '@babel/plugin-transform-runtime',
         {
           helpers: false
